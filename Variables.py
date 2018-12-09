4983eec0586586187a0575201a591e32664b80f7
Hakan Akın <cehakanakin@gmail.com>
2018-12-09 17:05:03 +0300

    Studied Variables
    I studied about variable types.

---
+++ Variables.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+"""
+
+## VARIABLES ##
+
+# %% Integer
+
+int1 = 10
+int2 = 15
+
+# %% Double and Float
+
+float1 = 10.33
+double1 = 45.58
+
+# %% String
+
+string1 = "Today is monday"
+string2 = "I'm Hakan"
+
+# %% Get the type of variable
+
+variable_type = type(string1)
+
+print(string1)
+
+string3 = string1 + " " +string2
+
+print(string3)
+
+var1 = "10"
+var2 = "20"
+var3 = var1 + var2
+
+print(var3)
+
+# %% Get the lenght of string
+
+length = len(string3)
+
+print(length)
+
+# %% Get the value of selected index
+
+print(string3[0])
+
+# %% Integer to Float
+
+int3 = 10
+
+intToFloat = float(int3)
+
+print(intToFloat)
+
+# %% Float to Integer
+
+float2 = 23.45
+
+floatToInt = int(float2)
+
+print(floatToInt)
+
+# %% String to Integer
+
+string4 = "11993"
+
+print(type(int(string4)))
+
